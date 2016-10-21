21569de94a88333a5412ebabb26fdf0802a6c1ef
Shengye Wang <i@shengye.wang>
2016-10-21 06:15:55 +0000

    Working

--- src/sendserver.cpp
+++ src/sendserver.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
@@ -13,9 +14,6 @@
 char buffer[1024];
 
 int main() {
-  RCSwitch sw;
-  sw.setPulseLength(DEFAULT_PULSE_LENGTH);
-  sw.enableTransmit(DEFAULT_PIN);
   int sd = socket(PF_INET, SOCK_DGRAM, 0);
   sockaddr_in addr;
   memset(&addr, sizeof(addr), 0);
@@ -26,16 +24,25 @@ int main() {
     perror("bind");
     exit(-1);
   }
+  if (wiringPiSetup () == -1) {
+    return EXIT_FAILURE;
+  }
   while (true) {
-    int addr_len = sizeof(addr);
-    int byte_cnt = 0;
+    unsigned addr_len = sizeof(addr);
+    unsigned byte_cnt = 0;
     byte_cnt = recvfrom(
         sd, buffer, sizeof(buffer), 0, (struct sockaddr*)&addr, &addr_len);
-    int code = 0;
-    for (int i = 0; i < 4; ++ i) {
-      code = (code << 8) | buffer[0];
+    if (byte_cnt == 4) {
+      int code = 0;
+      for (int i = 0; i < 4; ++ i) {
+        code = (code << 8) | buffer[i];
+      }
+      RCSwitch sw;
+      sw.setPulseLength(DEFAULT_PULSE_LENGTH);
+      sw.enableTransmit(DEFAULT_PIN);
+      sw.send(code, 24);
+      sendto(sd, buffer, byte_cnt, 0, (struct sockaddr*)&addr, sizeof(addr));
+      printf("Sent %d\n", code);
     }
-    sw.send(code, 24);
-    sendto(sd, buffer, bytes, 0, (struct sockaddr*)&addr, sizeof(addr));
   }
 }
